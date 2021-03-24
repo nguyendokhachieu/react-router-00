2086d86ae9d68426c6552247cef13417fdfa7fa9
nguyendokhachieu <nguyendokhachieu@gmail.com>
2021-03-24 10:49:04 +0700

    react-router init and Not Found component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  NavLink,
+  Switch
+} from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import NotFound from "./components/NotFound";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <div className="left">
+          <NavLink exact to="/" activeClassName="navLinkActive">
+            Home
+          </NavLink>
+          <br />
+          <NavLink exact activeClassName="navLinkActive" to="/about">
+            About
+          </NavLink>
+        </div>
+        <div className="right">
+          <Switch>
+            <Route exact path="/" component={Home}></Route>
+            <Route exact path="/about" component={About}></Route>
+            <Route component={NotFound}></Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
